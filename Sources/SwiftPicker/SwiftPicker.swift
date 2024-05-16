f44aff942124bf29e063c0f87117a378aa58d3fb
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-05-16 15:16:24 -0700

    WIP -> added SwiftPicker struct

---
+++ Sources/SwiftPicker/SwiftPicker.swift
@@ -0,0 +1,50 @@
+//
+//  SwiftPicker.swift
+//
+//
+//  Created by Nikolai Nobadi on 5/16/24.
+//
+
+public struct SwiftPicker {
+    public let padding: PickerPadding
+    
+    public init(padding: PickerPadding = .init()) {
+        self.padding = padding
+    }
+}
+
+
+// MARK: - SingleSelection
+public extension SwiftPicker {
+    func requiredSingleSelection<Item: DisplayablePickerItem>(title: String, items: [Item]) throws -> Item {
+        guard let selection = singleSelection(title: title, items: items) else {
+            throw SwiftPickerError.selectionCancelled
+        }
+        
+        return selection
+    }
+    
+    func singleSelection<Item: DisplayablePickerItem>(title: String, items: [Item]) -> Item? {
+        let info = makeInfo(title: title, items: items)
+        
+        return captureSingleInput(info: info, showNewScreen: true)
+    }
+}
+
+
+// MARK: - Private Methods
+private extension SwiftPicker {
+    func makeInfo<Item: DisplayablePickerItem>(title: String, items: [Item]) -> PickerInfo<Item> {
+        return .init(title: title, items: items, padding: padding)
+    }
+    func captureSingleInput<Item: DisplayablePickerItem>(info: PickerInfo<Item>, showNewScreen: Bool) -> Item? {
+//        let handler = PickerComposer.makeSingleSelectionPicker(info: info, newScreen: showNewScreen)
+//        let selection = handler.captureUserInput()
+//        
+//        handler.endSelection()
+//        printResult(selection?.displayName)
+//        
+//        return selection
+        return nil
+    }
+}
