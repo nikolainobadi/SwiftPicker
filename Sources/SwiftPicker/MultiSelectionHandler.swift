2a2c0ec8c5819c4ae09b498d4e585fa22a802003
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-05-16 15:39:34 -0700

    added MultiSelectionHandler

---
+++ Sources/SwiftPicker/MultiSelectionHandler.swift
@@ -0,0 +1,28 @@
+//
+//  MultiSelectionHandler.swift
+//  
+//
+//  Created by Nikolai Nobadi on 5/16/24.
+//
+
+internal final class MultiSelectionHandler<Item: DisplayablePickerItem>: BaseSelectionHandler<Item> {
+    func captureUserInput() -> [Item] {
+        scrollAndRenderOptions()
+        while true {
+            inputHandler.clearBuffer()
+            if inputHandler.keyPressed() {
+                if let char = inputHandler.readSpecialChar() {
+                    switch char {
+                    case .enter:
+                        return state.selectedOptions.map({ $0.item })
+                    case .space:
+                        state.toggleSelection(at: state.activeLine)
+                        scrollAndRenderOptions()
+                    }
+                }
+                
+                handleArrowKeys()
+            }
+        }
+    }
+}
