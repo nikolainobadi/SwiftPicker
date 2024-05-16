1f9d25f2bee9929b39978e743c59b65937af56d8
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-05-16 15:19:03 -0700

    added PickerInput protocol

---
+++ Sources/SwiftPicker/PickerInput.swift
@@ -0,0 +1,34 @@
+//
+//  PickerInput.swift
+//
+//
+//  Created by Nikolai Nobadi on 5/16/24.
+//
+
+internal protocol PickerInput {
+    func moveRight()
+    func moveToHome()
+    func clearBuffer()
+    func clearScreen()
+    func enableNormalInput()
+    func keyPressed() -> Bool
+    func write(_ text: String)
+    func exitAlternativeScreen()
+    func enterAlternativeScreen()
+    func moveTo(_ row: Int, _ col: Int)
+    func readDirectionKey() -> Direction?
+    func readSpecialChar() -> SpecialChar?
+    func readCursorPos() -> (row: Int, col: Int)
+    func readScreenSize() -> (rows: Int, cols: Int)
+}
+
+
+// MARK: - Dependencies
+internal enum Direction {
+    case up, down
+}
+
+internal enum SpecialChar {
+    case enter, space
+}
+
