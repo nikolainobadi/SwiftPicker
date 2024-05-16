cf49b478f9c7d4185ae3ff81cb4fe16f0e0d8dd0
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-05-16 15:24:28 -0700

    added SingleSelectionHandler

---
+++ Sources/SwiftPicker/SingleSelectionHandler.swift
@@ -0,0 +1,22 @@
+//
+//  SingleSelectionHandler.swift
+//  
+//
+//  Created by Nikolai Nobadi on 5/16/24.
+//
+
+internal final class SingleSelectionHandler<Item: DisplayablePickerItem>: BaseSelectionHandler<Item> {
+    func captureUserInput() -> Item? {
+        scrollAndRenderOptions()
+        while true {
+            inputHandler.clearBuffer()
+            if inputHandler.keyPressed() {
+                if let char = inputHandler.readSpecialChar(), char == .enter {
+                    return state.options.first(where: { $0.line == state.activeLine })?.item
+                }
+                
+                handleArrowKeys()
+            }
+        }
+    }
+}
