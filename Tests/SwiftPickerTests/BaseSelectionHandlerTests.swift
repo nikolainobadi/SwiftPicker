71aef132dad6718fc0e666866c946a4a28eaf206
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-05-17 15:28:13 -0700

    minor refactoring in unit tests

--- Tests/SwiftPickerTests/BaseSelectionHandlerTests.swift
+++ Tests/SwiftPickerTests/BaseSelectionHandlerTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import SwiftPicker
 
-final class BaseSelectionHandlerTests: XCTestCase {
+class BaseSelectionHandlerTests: XCTestCase {
     func test_init_startingValues() {
         let (_, input) = makeSUT(state: makeState())
         
@@ -29,24 +29,8 @@ final class BaseSelectionHandlerTests: XCTestCase {
     func test_scrollAndRenderOptions_startingValues() {
         let state = makeState()
         let (sut, input) = makeSUT(state: state)
-        let headerText = [state.topLineText, "\n", "\n", state.title, "\n"]
-        var displayableText: [String] = []
-        for i in 0..<20 {
-            displayableText.append("○")
-            displayableText.append(state.options[i].title)
-        }
-        let footerText = ["\n", "", "\n", state.bottomLineText]
-        let allText = headerText + displayableText + footerText
         
-        sut.scrollAndRenderOptions()
-    
-        XCTAssertEqual(state.activeLine, PickerPadding.top)
-        
-        allText.enumerated().forEach {
-            if !$1.isEmpty {
-                XCTAssertTrue(input.writtenText[$0].contains($1), "writtenText: \(input.writtenText[$0]) is not equal to \($1)")
-            }
-        }
+        assertWrittenText(sut: sut, input: input, expectedActiveLine: PickerPadding.top)
     }
     
     func test_handleArrowKeys_activeLineUpdates() {
@@ -74,6 +58,44 @@ final class BaseSelectionHandlerTests: XCTestCase {
         
         sut.handleArrowKeys()
         
+        assertWrittenText(sut: sut, input: input, expectedActiveLine: PickerPadding.top + 1)
+    }
+}
+
+
+// MARK: - Helper Methods
+extension BaseSelectionHandlerTests {
+    func makeItems() -> [String] {
+        return (1...25).map({ "Item \($0)" })
+    }
+}
+
+
+// MARK: - SUT
+fileprivate extension BaseSelectionHandlerTests {
+    func makeSUT(state: SelectionState<String>, screenSize: (Int, Int) = (26, 100), directionKey: Direction? = nil) -> (sut: BaseSelectionHandler<String>, input: MockInput) {
+        let input = MockInput(screenSize: screenSize, directionKey: directionKey)
+        let sut = BaseSelectionHandler(state: state, inputHandler: input)
+        
+        return (sut, input)
+    }
+    
+    func makeOptions(items: [String]) -> [Option<String>] {
+        return items.enumerated().map { .init(item: $1, line: PickerPadding.top + $0) }
+    }
+    
+    func makeState() -> SelectionState<String> {
+        let options = makeOptions(items: makeItems())
+        
+        return .init(options: options, topLine: PickerPadding.top, title: "This is my title", isSingleSelection: true)
+    }
+}
+
+
+// MARK: - Helper Assertions
+extension BaseSelectionHandlerTests {
+    func assertWrittenText(sut: BaseSelectionHandler<String>, input: MockInput, expectedActiveLine: Int, file: StaticString = #filePath, line: UInt = #line) {
+        let state = sut.state
         let headerText = [state.topLineText, "\n", "\n", state.title, "\n"]
         var displayableText: [String] = []
         for i in 0..<20 {
@@ -85,36 +107,14 @@ final class BaseSelectionHandlerTests: XCTestCase {
         
         sut.scrollAndRenderOptions()
     
-        XCTAssertEqual(state.activeLine, PickerPadding.top + 1)
+        XCTAssertEqual(state.activeLine, expectedActiveLine, "wrong active line", file: file, line: line)
         
         allText.enumerated().forEach {
             if !$1.isEmpty {
-                XCTAssertTrue(input.writtenText[$0].contains($1), "writtenText: \(input.writtenText[$0]) is not equal to \($1)")
+                XCTAssertTrue(input.writtenText[$0].contains($1), "writtenText: \(input.writtenText[$0]) is not equal to \($1)", file: file, line: line)
             }
         }
     }
 }
 
 
-// MARK: - SUT
-extension BaseSelectionHandlerTests {
-    func makeSUT(state: SelectionState<String>, screenSize: (Int, Int) = (26, 100), directionKey: Direction? = nil) -> (sut: BaseSelectionHandler<String>, input: MockInput) {
-        let input = MockInput(screenSize: screenSize, directionKey: directionKey)
-        let sut = BaseSelectionHandler(state: state, inputHandler: input)
-        
-        return (sut, input)
-    }
-    
-    func makeState() -> SelectionState<String> {
-        let options = makeOptions(items: makeItems())
-        return .init(options: options, topLine: PickerPadding.top, title: "This is my title", isSingleSelection: true)
-    }
-    
-    func makeItems() -> [String] {
-        return (1...25).map({ "Item \($0)" })
-    }
-    
-    func makeOptions(items: [String]) -> [Option<String>] {
-        return items.enumerated().map { .init(item: $1, line: PickerPadding.top + $0) }
-    }
-}
