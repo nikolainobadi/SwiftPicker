727c866ae2cc6ecd883caeff54d9bc5cee58b1f8
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-05-18 12:52:15 -0700

    added docs for Option, PickerInfo, PickerPadding, and SelectionState

--- Sources/SwiftPicker/Internal/Models/PickerPadding.swift
+++ Sources/SwiftPicker/Internal/Models/PickerPadding.swift
@@ -5,7 +5,12 @@
 //  Created by Nikolai Nobadi on 5/16/24.
 //
 
+/// An enumeration defining the padding to be used in the `SwiftPicker` selection list.
+/// This includes the top and bottom padding values.
 internal enum PickerPadding {
+    /// The number of lines to pad at the top of the selection list.
     static let top = 4
+    
+    /// The number of lines to pad at the bottom of the selection list.
     static let bottom = 2
 }
